b2c57dbaf9ad9b8d136d298bc1f284b3be29e26e
Iverganz <33731524+Iverganz@users.noreply.github.com>
2021-01-21 16:36:01 +0400

    Добавлена консольная утилита.

---
+++ ConsoleAppTrainProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TrainProblem;
+
+namespace ConsoleAppTrainProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var len = 459;
+            var Train = new Train(len);
+            var count = 0;
+            var car = Train.Head;
+            Console.WriteLine("Состав поезда: ");
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(car.LightOn.ToString() + ' ');
+                car = car.Next;
+            }
+            
+            Console.WriteLine("В поезде: " + Train.CountByLights(Train.Head).ToString() + " вагонов.");
+            Console.ReadLine();
+        }
+    }
+}
